4aa545eb9f894744d43a5eb9729fc2c42947f5c7
Kanhaiya Lal <104843240+kanhaiya1208@users.noreply.github.com>
2023-07-10 15:13:29 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const Axios = require("axios");
 const qs=require('qs');
 const app = express();
-const PORT = process.env.PORT ||8000;
+const PORT = process.env.PORT ||3000;
  const path=require('path');
 app.use(cors());
 app.use(express.json());
